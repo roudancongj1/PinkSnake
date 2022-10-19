befd7777acf71238f078c93e558e890730f99fbd
ggao4 <guangyun.gao2@dxc.com>
2022-10-19 20:49:08 +0800

    pom 结构修改

--- OrderServer/src/main/java/com/pk/orderserver/OrderServerApplication.java
+++ OrderServer/src/main/java/com/pk/orderserver/OrderServerApplication.java
@@ -3,6 +3,7 @@ package com.pk.orderserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.sql.init.dependency.DatabaseInitializationDependencyConfigurer;
 
 //@SpringBootApplication(scanBasePackages = { "com.pk" },exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication(scanBasePackages = { "com.pk" })
